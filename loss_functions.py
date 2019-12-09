3108fedbb207237dd8ca84b215a50c4e15549eec
Alex Harry <alex.harry1@gmail.com>
2019-12-08 20:46:46 -0700

    Changed loss function to previous working code

--- loss_functions.py
+++ loss_functions.py
@@ -1,55 +1,35 @@
-"""
-Justin Keeling
-Alex Harry
-John Lambrecht
-Andrew Smith
-
-Institution: Montana State University
-Course: CSCI-447 Machine Learning
-Instructor: John Shepherd
-
-File:  Contains the loss functions for the machine learning algorithms to see how the model performs
-"""
-
+import data as data
 import pandas as pd
-
+from PAM import KNN
 import math
 
 
 class LF:
 
-    def zero_one_loss(self, data_set, k_val, name, in_data):
+    def zero_one_loss(self, predicted, labels):
+
             """
                  Condense the data set by instantiating a Z = None. Add x_initial to Z if initial class(x_initial) != class(x)
                  where x is an example in Z.
                  So: Eliminates redundant data.
-                 :param data_set: data we want to reduce.
-                 :param k_val: # of neighbors, used when performing knn
-                 :param name: name of data set we are working with (for knn)
-                 :param in_data:  data object to perform operations in process_data class
-                 :return: the zero one less (accuracy) of KNN on a given set of data
+                 :param predicted: Predicted labels
+                 :param labels: actual labels
+                 :return: the zero one less type float
              """
-            knn = KNN()
+            for iterator in range(len(predicted)):
+                predicted[iterator] = round(predicted[iterator])
+            length_of_labels = len(labels) # Length of entire predicted dataset
             zero_count = 0  # count of correctly classified examples
             one_count = 0  # count of incorrectly classified examples
-            class_col = in_data.get_label_col(name)  # column that class values will be stored in for given dataset
-            print("\n-----------------Performing Zero One Loss-----------------")
-
-            # go through each datapoint (row) and check what class KNN thinks it is
-            for index, row in data_set.iterrows():
-
-                knn_class = knn.perform_knn(row, data_set, k_val,name, in_data)
-                actual_class = row[class_col]
-                # if they match increment the zero count
-                print("KNN class: ", knn_class, " ACTUAL CLASS: ", actual_class)
-                if int(knn_class) == int(actual_class):
+            # Following code for using zip to loop along two lists source: https://stackoverflow.com/questions/1919044/is-there-a-better-way-to-iterate-over-two-lists-getting-one-element-from-each-l, user: sateesh, Agostino
+            for (predict, label) in zip(predicted, labels):  # go through all the points at the same time
+                if predict == label:
                     zero_count += 1
                 else:
-                    one_count += 1   # else increment one count
-
-            print("\n--- KNN Classified ", zero_count, "Examples Correctly and ", one_count, "Incorrectly---")
-            print("\n--- With total Zero One Loss of: ", (zero_count/one_count), "---")
-            return zero_count/one_count  # total accuracy
+                    one_count += 1
+            # print("\n--- KNN Classified", zero_count, "Examples Correctly and", one_count, "Incorrectly---")
+            print("\n--- Zero One Loss:", (one_count/length_of_labels), "---")
+            return one_count/length_of_labels  # Return zero one loss
 
 
 
@@ -63,10 +43,13 @@ class LF:
         n = len(actual_data)  # get out n for MSE
         sum = 0  # sum of the MSE squared values
 
-        for (predict, true) in (predicted_data, actual_data): # go through all the points at the same time
+        # Following code for using zip to loop along two lists source: https://stackoverflow.com/questions/1919044/is-there-a-better-way-to-iterate-over-two-lists-getting-one-element-from-each-l, user: sateesh, Agostino
+        for (predict, true) in zip(predicted_data, actual_data): # go through all the points at the same time
             currentSum = (true - predict) ** 2  # square it
             sum += currentSum # add current to total sum
 
         # divide by n
         sum = sum/n
-        return sum # done, return sum
+        print("\n--- Mean Squared Error:", sum,  "---\n")
+
+        return sum # done, return su
